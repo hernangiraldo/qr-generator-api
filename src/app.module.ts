a3ce5000b1904e51270e0e591908497a7a48b879
Hernán <hernan.giraldo.giraldo@gmail.com>
2023-12-30 06:50:07 -0400

    Remove CipherModule and TokenModule imports from AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,8 @@ import { QrModule } from './qr/qr.module';
 import { DbModule } from './db/db.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import { CipherModule } from './cipher/cipher.module';
-import { TokenModule } from './token/token.module';
 
 @Module({
-  imports: [QrModule, AuthModule, UsersModule, DbModule, CipherModule, TokenModule],
+  imports: [DbModule, QrModule, AuthModule, UsersModule],
 })
 export class AppModule {}
